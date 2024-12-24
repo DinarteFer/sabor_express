24d8c969a2bbf179fdefa1e4bd73146e25bf946c
Dinarte Fernandes <dinartevinicius@ymail.com>
2024-12-24 17:30:53 -0300

    Resolvendo opções inválidas

--- app.py
+++ app.py
@@ -20,20 +20,31 @@ def exibir_opcoes():
 def finalizar_app():
     print('Finalizando o app\n')
 
-def escolher_opcao():
-    opcao_escolhida = int(input('Escolha uma opção: '))
-    # opcao_escolhida = int(opcao_escolhida)
-    if opcao_escolhida == 1:
-        print ('Cadastrar restaurante')
-    elif opcao_escolhida == 2:
-        print ('Listar restaurante')
-    elif opcao_escolhida == 3:
-        print ('Ativar restaurante')
-    else:
-        os.system('cls')
-        finalizar_app()
+def opcao_invalida():
+    print('Opção inválida!\n')
+    input('Digite uma tecla para voltar')
+    main()
 
+def escolher_opcao():
+    try:
+        opcao_escolhida = int(input('Escolha uma opção: '))
+        # opcao_escolhida = int(opcao_escolhida)
+        if opcao_escolhida == 1:
+            print ('Cadastrar restaurante')
+        elif opcao_escolhida == 2:
+            print ('Listar restaurante')
+        elif opcao_escolhida == 3:
+            print ('Ativar restaurante')
+        elif opcao_escolhida == 4:
+            os.system('cls')
+            finalizar_app()
+        else:
+            opcao_invalida()
+    except:
+        opcao_invalida()
+        
 def main():
+    os.system('cls')
     exibir_nome_do_programa()
     exibir_opcoes()
     escolher_opcao()
